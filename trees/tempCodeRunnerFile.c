8bc155699c025b015f5f66648b8c073197d81cf1
jashhh0908 <jashunik9@gmail.com>
2025-11-09 13:32:53 +0530

    deleted temp and exe files

--- trees/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-void inorder(struct Node* root) {
-    struct Node* temp = root;
-    if(temp == NULL) return;
-    inorder(temp->left);
-    printf("%d ", temp->data);
-    inorder(temp->right);
-}

6414b63b0f41a68fa01a76ead33c8e7a7c83be75
jashhh0908 <jashunik9@gmail.com>
2025-11-09 12:37:34 +0530

    added merge sort code for college exam

---
+++ arrays/sorting/merge_sort_clg.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void display(int *a, int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}
+
+void merge(int *a, int mid, int low, int high) {
+    int i,j,k,b[100];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while(i <= mid && j <= high) {
+        if(a[i] < a[j]){
+            b[k] = a[i];
+            i++;
+        }
+        else {
+            b[k] = a[j];
+            j++;
+        }
+        k++;
+    }
+    while(i <= mid) {
+        b[k] = a[i];
+        i++; k++;
+    }
+    while(j <= high) {
+        b[k] = a[j];
+        j++; k++;
+    }
+
+    for(i = low; i <= high; i++) {
+        a[i] = b[i];
+    }
+}
+
+void mergeSort(int *a, int low, int high) {
+    int mid;
+    if(low < high) {
+        mid = (low + high) / 2;
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+        merge(a, mid, low, high);
+    }
+}
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter array elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Before Sort:-\n");
+    display(a,n);
+    mergeSort(a, 0, n-1);
+    printf("\nAfter Sort:-\n");
+    display(a,n);
+    return 0;
+}
